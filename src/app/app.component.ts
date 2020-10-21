80606e458330154bb3015e8e57d7f98413d4d815
miscing <miscing-public@pm.me>
2020-10-21 19:16:00 +0300

    added profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import rawData from './data.json';
+import { Cv } from './cv';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cv';
+	data :Cv = rawData;
+	constructor() {
+	}
 }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,18 @@
+import {  Component, Input, OnInit } from '@angular/core';
+
+import { Profile } from '../cv';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+	@Input() profile :Profile;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
