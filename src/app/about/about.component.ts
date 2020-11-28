994cdab6c907cfae1b6e5716458b8efc9a9503c8
miscing <miscing-public@pm.me>
2020-11-28 03:53:25 +0200

    added about section code

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,18 @@
+import { Input, Component, OnInit } from '@angular/core';
+
+import { About  } from '../cv';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+	@Input() about :About;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
